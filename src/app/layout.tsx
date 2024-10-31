d977a8e814658ff1001b7c8b3187b840ad7e9ddc
lsy0xcc <lsy0xcc@hotmail.com>
2024-10-31 16:01:40 +0800

    chore: update file structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,12 +5,12 @@ import '@radix-ui/themes/styles.css';
 import { Theme } from '@radix-ui/themes';
 
 const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
+  src: '../fonts/GeistVF.woff',
   variable: '--font-geist-sans',
   weight: '100 900',
 });
 const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
+  src: '../fonts/GeistMonoVF.woff',
   variable: '--font-geist-mono',
   weight: '100 900',
 });
