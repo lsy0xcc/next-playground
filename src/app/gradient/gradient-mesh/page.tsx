f9da1d6cfabe84c930afa07764d90a04249caacc
lsy0xcc <lsy0xcc@hotmail.com>
2024-11-07 22:33:49 +0800

    feat: update moving gradient mesh style

--- src/app/gradient/gradient-mesh/page.tsx
+++ src/app/gradient/gradient-mesh/page.tsx
@@ -1,26 +1,37 @@
 'use client';
 import { Button } from '@radix-ui/themes';
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import { useEffect } from 'react';
 
 export default function GradientMeshPage() {
   const router = useRouter();
 
-  const [step, setStep] = useState(0);
-  const changeStep = () => {
-    setStep((old) => (old + 1) % 5);
-  };
   const jumpToToolWebsite = () => {
     router.push('https://csshero.org/mesher/');
   };
 
+  useEffect(() => {
+    const mouseMoveHandler = (e: MouseEvent) => {
+      document.body.style.setProperty(
+        '--x-ratio',
+        `${e.x / window.screen.width}%`,
+      );
+      document.body.style.setProperty(
+        '--y-ratio',
+        `${e.y / window.screen.height}%`,
+      );
+    };
+    window.addEventListener('mousemove', mouseMoveHandler);
+    return () => {
+      document.body.style.removeProperty('--x-ratio');
+      document.body.style.removeProperty('--y-ratio');
+      window.removeEventListener('mousemove', mouseMoveHandler);
+    };
+  });
+
   return (
-    <div className={'h-screen w-screen ' + `bg-step-${step}`}>
-      <div
-        className={
-          'relative h-full w-full ' + (step >= 4 ? 'backdrop-blur-2xl' : '')
-        }
-      >
+    <div className="gradient-mesh-bg h-screen w-screen">
+      <div className="relative h-full w-full backdrop-blur-2xl">
         <div className="absolute top-[40%] w-full gap-8 space-y-8 px-12 sm:left-[20%] sm:w-[480px] sm:px-0">
           <div className="text-[4rem] font-extrabold leading-none">
             Lorem Ipsum
@@ -29,22 +40,14 @@ export default function GradientMeshPage() {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. In et
             sapien laoreet nisl tempus sagittis. Ut diam justo, euismod et
             interdum ornare, porttitor sit amet tortor. Donec elementum laoreet
-            ipsum, id aliquam dolor sodales in. {step}
+            ipsum, id aliquam dolor sodales in.
           </div>
           <div className="space-x-4">
             <Button
               variant="solid"
               radius="full"
               size="3"
-              highContrast
-              onClick={changeStep}
-            >
-              Next Step
-            </Button>
-            <Button
-              variant="outline"
-              radius="full"
-              size="3"
+              color="gray"
               highContrast
               onClick={jumpToToolWebsite}
             >
